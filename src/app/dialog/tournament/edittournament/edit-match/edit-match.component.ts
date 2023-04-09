1a1f6726dce04609a4bdddacdbf763a40fc6f24a
nrlowe <nate.lowe.2013@owu.edu>
2023-04-08 20:07:58 -0400

    Adding dialog module to better organize/structure app + added edit-match dialog

---
+++ src/app/dialog/tournament/edittournament/edit-match/edit-match.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Inject } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatchNode } from 'src/app/models/matchnode';
+
+@Component({
+  selector: 'app-edit-match',
+  templateUrl: './edit-match.component.html',
+  styleUrls: ['./edit-match.component.css']
+})
+export class EditMatchComponent {
+  constructor(
+    public dialogRef: MatDialogRef<EditMatchComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: MatchNode,
+  ) {}
+  
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
